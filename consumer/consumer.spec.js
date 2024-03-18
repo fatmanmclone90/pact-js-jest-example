28cb94a34865a5f195b7015ea52d5d6e9bd7fbad
chris_tamlyn <Chris_Tamlyn@next.co.uk>
2024-03-18 10:04:27 +0000

    using jest-pact

--- consumer/consumer.spec.js
+++ consumer/consumer.spec.js
@@ -1,54 +1,30 @@
-const path = require('path');
+const { pactWith } = require ('jest-pact/dist/v3');
 const { getProducts } = require('./productClient.js');
-const { PactV3, MatchersV3 } = require('@pact-foundation/pact');
+const { allProductsResponse } = require('./pact.fixtures.js');
 
-const {
-  atLeastLike,
-  eachLike,
-  like,
-  integer,
-  string,
-} = MatchersV3;
-
-const provider = new PactV3({
-  dir: path.resolve(process.cwd(), 'pacts'),
-  consumer: 'APIConsumer',
-  provider: 'ProductsAPI',
-});
-
-const EXPECTED_PRODUCT_LIKE = { 
-    id: integer(), // random but between 0 to 10
-    name: string()
-}; 
-
-describe('Products API', () => {
-  describe('When a GET request is made to /products', () => {
-    test('it should return all products', async () => {
+pactWith({ consumer: 'APIConsumer', provider: 'ProductsAPI' }, (interaction) => {
+  interaction('When a GET request is made to /products', ({ provider, execute }) => {
+    beforeEach(() =>
       provider
-        .uponReceiving('a request to all products')
-        .withRequest({
+        .given('Products exist')
+        .uponReceiving('A request for products')
+          .withRequest({
           method: 'GET',
           path: '/products',
           headers: { Accept: 'application/json' },
         })
-        .willRespondWith({
-          status: 200,
-          body: eachLike(EXPECTED_PRODUCT_LIKE),
-          headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-          }
-        })
+        .willRespondWith(allProductsResponse)
+    );
 
-      await provider.executeTest(async mockServer => {
-        process.env.PROVIDER_API_PORT = mockServer.port
-        const products = await getProducts();
+    execute('All products are returned', async (mockServer) => {
+      process.env.PROVIDER_API_PORT = mockServer.port;
+      const products = await getProducts();
 
-        products.forEach(product => { 
-          expect(product.id).toBeGreaterThanOrEqual(1);
-          expect(product.name).toBeTruthy();
-        });
-        expect(products.length).toBe(1);
+      products.forEach(product => { 
+        expect(product.id).toBeGreaterThanOrEqual(1);
+        expect(product.name).toBeTruthy();
       });
-    });
+      expect(products.length).toBe(1);
+    })
   });
 });
